39d190acb6a1a90924f2f6aefa3c23eeddf6d75c
deepikasai76 <72790336+deepikasai76@users.noreply.github.com>
2023-08-26 23:41:22 +0100

    Update chat.py

--- chat.py
+++ chat.py
@@ -13,10 +13,17 @@ import chromadb
 from chromadb.config import Settings
 import streamlit as st
 import app
+import django
+
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # # Set the page configuration
 # st.set_page_config(page_title="🦜️🔗Langchain PDF Chatbot 🤖", layout='centered')
 
