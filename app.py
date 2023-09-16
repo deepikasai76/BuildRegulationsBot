359aca4ccd23ee566344aa3258274b1ca749829b
deepikasai76 <72790336+deepikasai76@users.noreply.github.com>
2023-09-16 20:02:00 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -2,22 +2,15 @@ import chat
 import streamlit as st
 from streamlit_chat import message
 
-
-
-
-# # The page configuration for the app
-# st.set_page_config(page_title="🦜️🔗Langchain PDF Chatbot 🤖", layout='centered')
-
 # The title with HTML content
 title_html = """
-<h1>BuildRegulationsBot: LLM-Powered 🦜️🔗 Merged Docs Assistance 
+<h1>BuildRegulationsBot 🤖: LLM-Powered 🦜️🔗 Merged Docs Assistance 
 <i class="fa-sharp fa-solid fa-user-helmet-safety"></i></h1>
 """
 st.markdown(title_html, unsafe_allow_html=True)
 
 # Subheader to provide additional information
 st.subheader("Guiding You through Merged Approved Documents of Building Regulations using Langchain🦜️🔗")
-
 
 # Initialize session_state variables to store chat history
 if 'generated' not in st.session_state:
@@ -34,11 +27,12 @@ def clear_input_text():
 # Get the user's input by calling the get_text function
 def get_text():
     global input_text
-    input_text = st.text_input("Ask your Question", key="input", on_change=clear_input_text)
+    input_text = st.text_input("Ask your Question", key="input",
+                               placeholder="Ask your questions about the document!", on_change=clear_input_text)
     return input_text
 
 def main():
-
+    
     # Get user input
     user_input = get_text()
 
@@ -52,6 +46,7 @@ def main():
 
      # Display chat history
     if st.session_state['generated']:
+    
         for i in range(len(st.session_state['generated'])-1, -1, -1):
             # Display bot's response
             message(st.session_state["generated"][i], key=str(i))
